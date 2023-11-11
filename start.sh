123fc8874387176a73bffa1271fcc195f145c78d
Candypop678 <127382325+Candypop678@users.noreply.github.com>
2023-11-11 14:49:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Candypop678 /DQ-_TOM-New-Version-Nov
+  git clone https://github.com/Candypop678/DQ-_TOM-New-Version-Nov /DQ-_TOM
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-_TOM-New-Version-Nov
+  git clone $UPSTREAM_REPO /DQ-_TOM
 fi
 cd /DQ-_TOM
 pip3 install -U -r requirements.txt
-echo "Starting DQ-_TOM-New-Version-Nov...."
+echo "Starting DQ-_TOM...."
 python3 bot.py
